22748997396c2fcff03f8c04dba1c7eda64f7ca3
AndrewChacon <andrew.b.chacon@gmail.com>
2020-05-28 10:52:38 -0400

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Get All Pokemon Info - https://pokeapi.co/api/v2/pokemon/1
+// Get Pokemon Form Info - https://pokeapi.co/api/v2/pokemon-form/1
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemonData: [],
+    };
+  }
+
+  componentDidMount() {
+    for (let i = 1; i < 152; i++) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then((response) => response.json())
+        .then((pokemon) => {
+          this.setState({ pokemonData: [...this.state.pokemonData, pokemon] });
+        });
+    }
+    this.setState({
+      pokemonData: this.state.pokemonData.sort((a, b) =>
+        a.id > b.id ? 1 : -1
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Pokedex Application</h1>
+          {this.state.pokemonData.map((pokemon) => (
+            <div className="card" key={pokemon.id}>
+              <h2>{pokemon.name}</h2>
+              <h5>{`Pokemon Entry #${pokemon.id}`}</h5>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+              <img
+                src={pokemon.sprites.front_shiny}
+                alt={`shiny ${pokemon.name}`}
+              />
+              {pokemon.types.map((types) => (
+                <p
+                  key={types.type.name}
+                  className={`types-text ${types.type.name}`}
+                >
+                  {types.type.name.toUpperCase()}
+                </p>
+              ))}
+            </div>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
